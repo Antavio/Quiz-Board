cd7c3f6f1eb5658e3d9157edc91be7ec782b7161
Antavio <njuguna13@gmail.com>
2019-02-22 17:24:14 +0300

    Added scripts to access user data.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 function addition(q1,q2,q3,q4){
   return q1+q2+q3+q4;
 }
+$(document).ready(function(){
+  $(".question-header").click(function(){
+    $(".multi-choices").slideToggle("slow");
+  });
+  $("#formid").submit(function(event){
+    event.preventDefault();
+    var q1 = parseInt($("input:radio[name=question-one]:checked").val());
+    var q2 = parseInt($("input:radio[name=question-two]:checked").val());
+    var q3 = parseInt($("input:radio[name=question-three]:checked").val());
+    var q4 = parseInt($("input:radio[name=question-four]:checked").val());
+
+    var final = addition(q1,q2,q3,q4);
+    var percent = (final/60)*100;
+     $("#results").text("You scored " + percent + "%");
+    //alert("You scored " + final + "%");
+    
+  });
+});
